cd72cffdb6cb5bb7f4dffed490fc518f38b63c51
Kosuke0505 <77870757+Kosuke0505@users.noreply.github.com>
2022-07-20 06:47:31 +0900

    Step1実装

---
+++ Unity_Byoshitsu/Assets/04_Script/01_GameScript/02_TapScript/Objects/Box2_Judge.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box2_Judge : MonoBehaviour
+{
+    //正解したかどうか
+    public bool isClear = false;
+
+    //ボタンの4桁
+    public string InputNo = "0000";
+
+    //答えの4桁
+    public string AnswerNo = "2313";
+
+    //OpenCover&CloseCoverオブジェクト
+    public GameObject OpenCover;
+    public GameObject CloseCover;
+    //MedicinePackCollider定義
+    //public GameObject MedicinePackColiider;
+
+
+
+    //答え合わせ
+    public void JudgeAnswer(string buttonName, int Index)
+    {
+        //入力値を更新
+        if (buttonName == "Btn1") //ボタン1の時
+        {
+            //1桁目をチェンジ
+            InputNo = Index + InputNo.Substring(1);
+        }
+        else if (buttonName == "Btn2") //ボタン2の時
+        {
+            //2桁目をチェンジ
+            InputNo = InputNo.Substring(0, 1) + Index + InputNo.Substring(2);
+        }
+        else if (buttonName == "Btn3") //ボタン3の時
+        {
+            //3桁目をチェンジ
+            InputNo = InputNo.Substring(0, 2) + Index + InputNo.Substring(3);
+        }
+        else //ボタン4の時
+        {
+            //4桁目をチェンジ
+            InputNo = InputNo.Substring(0, 3) + Index;
+        }
+
+
+        //答え判定
+        if (InputNo == AnswerNo)
+        {
+            //クリアの効果音
+            AudioManager.Instance.SoundSE("Clear");
+            //クリア判定をtrueに
+            isClear = true;
+
+            //画面ブロック
+            BlockPanel.Instance.ShowBlock();
+
+            //1秒後にカメラ移動
+            Invoke(nameof(AfterClear1), 1);
+
+            //最後にセーブ
+            SaveLoadSystem.Instance.Save();
+        }
+
+    }
+
+
+
+    //正解後のカメラ移動
+    private void AfterClear1()
+    {
+        //カメラ移動
+        CameraManager.Instance.ChangeCameraPosition("Box");
+        //1秒後にスライド開く
+        Invoke(nameof(AfterClear2), 1.5f);
+    }
+
+    //正解後のスライド開く
+    private void AfterClear2()
+    {
+        //効果音
+        AudioManager.Instance.SoundSE("Slide");
+        //Close→Open
+        CloseCover.SetActive(false);
+        OpenCover.SetActive(true);
+        //MedicinePackCollider解除
+        //MedicinePackColider.SetActive(true);
+        //画面ブロックを解除
+        BlockPanel.Instance.HideBlock();
+    }
+}
---
+++ Unity_Byoshitsu/Assets/04_Script/01_GameScript/02_TapScript/Objects/Box2_Tap.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box2_Tap : TapCollider
+{
+
+    //ボタン名
+    public string ButtonName;
+    //インデックス
+    public int Index = 0;
+    //オブジェクト配列
+    public GameObject[] Objects;
+
+    //答え合せクラス
+    public Box2_Judge JudgeClass;
+
+
+    //ボタンタップ時
+    protected override void OnTap()
+    {
+        base.OnTap();
+
+        //答えが正解済みの場合は処理しない
+        if (JudgeClass.isClear)
+            return;
+
+        //効果音
+        AudioManager.Instance.SoundSE("TapButton");
+
+        //表示中の画像を非表示に
+        Objects[Index].SetActive(false);
+
+        //インデックスを+1
+        Index++;
+
+        //インデックスがオブジェクト配列の要素数と同じ以上の場合、0に戻る
+        if (Index >= Objects.Length)
+            Index = 0;
+
+        //次の画像を表示
+        Objects[Index].SetActive(true);
+
+        //ボタンを後ろに移動
+        this.gameObject.transform.Translate(new Vector3(0, 0.02f, 0));
+
+        //0.1秒後にボタン位置を元に戻す
+        Invoke(nameof(delayButton), 0.1f);
+
+        //答え合せ
+        JudgeClass.JudgeAnswer(ButtonName, Index);
+    }
+
+
+
+    //押されたボタンを戻す
+    private void delayButton()
+    {
+        this.gameObject.transform.Translate(new Vector3(0, -0.02f, 0));
+    }
+}
